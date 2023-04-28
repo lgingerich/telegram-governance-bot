fab17a186c0c914bf7450a0bab23f74167563e56
Landon Gingerich <Landon>
2023-04-27 20:52:41 -0500

    add GraphQL API support to fetch full proposal data

--- webhook-cloud-function/main.py
+++ webhook-cloud-function/main.py
@@ -1,15 +1,65 @@
 import json
+import time
 from google.cloud import firestore
+import requests
 from flask import jsonify
 
 db = firestore.Client()
 
-def store_event(event_data):
-    event_ref = db.collection("events").document()
-    event_data["timestamp"] = firestore.SERVER_TIMESTAMP
-    event_ref.set(event_data)
+def store_event(data):
+    event_id = data["id"]
+    doc_ref = db.collection("events").document(event_id)
+    doc_ref.set(data)
+    return event_id
 
-    return event_ref.id
+def fetch_proposal_data(proposal_id, max_retries=3, delay=2):
+    url = 'https://hub.snapshot.org/graphql'
+    query = '''
+    query ($id: String!) {
+      proposal(id: $id) {
+        id
+        title
+        body
+        choices
+        start
+        end
+        snapshot
+        state
+        author
+        created
+        scores
+        scores_by_strategy
+        scores_total
+        scores_updated
+        plugins
+        network
+        strategies {
+          name
+          network
+          params
+        }
+        space {
+          id
+          name
+        }
+      }
+    }
+    '''
+
+    variables = {
+        'id': proposal_id
+    }
+
+    for i in range(max_retries):
+        try:
+            response = requests.post(url, json={'query': query, 'variables': variables})
+            data = response.json()
+            return data['data']['proposal']
+        except Exception as e:
+            if i < max_retries - 1:
+                time.sleep(delay)
+            else:
+                raise e
 
 def webhook(request):
     if request.method == 'POST':
@@ -22,6 +72,19 @@ def webhook(request):
             'expire': data['expire']
         }
 
-        event_id = store_event(event_data)
+        # Fetch the additional proposal data
+        proposal_id = event_data['id'].split('/')[-1]
+        
+        try:
+            proposal_data = fetch_proposal_data(proposal_id)
+        except Exception as e:
+            return jsonify({"status": "error", "message": f"Error fetching proposal data: {str(e)}"})
+
+        # Merge the event data and the proposal data
+        merged_data = {**event_data, **proposal_data}
+
+        # Store the merged data in Firestore
+        event_id = store_event(merged_data)
 
         return jsonify({"status": "OK"})
+
