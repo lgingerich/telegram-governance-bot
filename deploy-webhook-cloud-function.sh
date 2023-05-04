6d00fbbf9ef8569054d53be2d8f2e676b52d320c
Landon Gingerich <Landon>
2023-05-04 03:10:04 +0000

    initial commit

---
+++ deploy-webhook-cloud-function.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -xe
+
+gcloud functions deploy snapshot_webhook \
+  --gen2 \
+  --region=us-central1 \
+  --runtime=python310 \
+  --source=./webhook-cloud-function \
+  --entry-point=webhook \
+  --trigger-http
